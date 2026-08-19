3dc3ee3abf7d372dba46e77107a250538b434fd6
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-19 12:12:08 +0800

    Just add the option

--- BarInventoryChecker.py
+++ BarInventoryChecker.py
@@ -1,3 +1,12 @@
 print("------------------------------")
 print("*** Bar Inventory Checker ***")
 print("------------------------------")
+
+print("1. View Inventory")
+print("2. Check Item Stock")
+print("3. Add Stock")
+print("4. Remove Stock")
+print("5. Check Low Stock")
+print("6. Search Item")
+print("7. Show Total Items")
+print("8. Exit")

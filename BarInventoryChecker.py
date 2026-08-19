51003b1763854f6f0e80118e9cd357c285e8741f
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-20 01:43:43 +0800

    create getMenu function to handle options input

--- BarInventoryChecker.py
+++ BarInventoryChecker.py
@@ -1,25 +1,46 @@
-print("------------------------------")
-print("*** Bar Inventory Checker ***")
-print("------------------------------")
+liquor = [
+    "Vodka",
+    "Gin",
+    "Rum",
+    "Tequila",
+    "Whiskey",
+    "Brandy",
+]
 
-print("1. View Inventory")
-print("2. Check Item Stock")
-print("3. Add Stock")
-print("4. Remove Stock")
-print("5. Check Low Stock")
-print("6. Search Item")
-print("7. Show Total Items")
-print("8. Exit")
-print("\nPlease select an option from the menu above.")
-
-
-menuOption = int(input("Enter your option: "))
+def getMenu():
+    print("------------------------------")
+    print("*** Bar Inventory Checker ***")
+    print("------------------------------")
+    print("1. View Inventory")
+    print("2. Check Item Stock")
+    print("3. Add Stock")
+    print("4. Remove Stock")
+    print("5. Check Low Stock")
+    print("6. Search Item")
+    print("7. Show Total Items")
+    print("8. Exit")
+    print("\nPlease select an option from the menu above.")
+    return int(input("Enter your option: "))
 
 while True:
+    menuOption = getMenu()
     if menuOption <= 1 or menuOption <= 8:
-        print("Correct option!")
-    else: print("Incorrect option!")
-    break
+        if menuOption == 1:
+            print("---> View Inventory <---")
+            print("\n".join(liquor))
+            getMenu()
+        elif menuOption == 3:
+            add_list = input("Add Stock: ")
+            liquor.append(add_list)
+            print("\n".join(liquor))
+        elif menuOption == 4:
+            remove_list = input("Remove Stock: ")
+            liquor.remove(remove_list)
+            print("\n".join(liquor))
+    else: 
+        print("Incorrect option!")
+        getMenu()
+    
 
 print("Program closed!")
 

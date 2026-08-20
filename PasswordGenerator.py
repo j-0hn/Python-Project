ff19a43236dd2d5db2c8397c2c09656ee594e2a0
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-21 02:13:58 +0800

    update my code, remove list[], fix the the output that always show the final print that supposed to be not

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -7,36 +7,36 @@ varLetters = string.ascii_letters
 varDigits = string.digits
 varSpecialChars = string.punctuation
 
-passWord_list = []
+passWord = ""
 
 def get_input_range():
-    return int(input("How many characters do you want in your password? "))
+    return int(input("\nHow many characters do you want in your password? "))
 
-generate = True
+while True:
+    try:
+        get_range_password = get_input_range()
+        if get_range_password < 4:
+            print("Password must be at least 4 characters long!")
 
-while generate:
+        elif get_range_password > 8:
+            print("Password must not exceed 8 characters!")
+        else:
+            for i in range(get_range_password):
+                passWord += random.choice(varLetters + varDigits + varSpecialChars)
+                    
+            print("\nYour Password is: ", passWord)
+            print("Total Characters: ", len(passWord))
+            passWord = ""
 
-    while True:
-        try:
-            get_range_password = get_input_range()
-            if get_range_password < 4:
-                print("Password must be at least 4 characters long!")
-                
-            elif get_range_password > 8:
-                print("Password must not exceed 8 characters!")
-
+            print("\nEnter Y / N")
+            regenerate = input("Generate Again? : ")
+            if regenerate == "Y" or regenerate == "y":
+                continue #this will continue the loop and ask agin inputs
             else:
-                for i in range(get_range_password):
-                    i = random.choice(varLetters + varDigits + varSpecialChars)
-                    passWord_list.append(i)
+                break
 
-            random.shuffle(passWord_list)
-            print("\nYour Password is: " + "".join(passWord_list))
-            print("Total Characters: " + str(len("".join(passWord_list))))
-            passWord_list.clear() #this is to clear the list so that it can be used again for another password generation
-            generate = False
-        except ValueError:
-            print("Please enter a valid number!")
-
+    except ValueError:
+        print("Please enter a valid number!")
+        
 print("Please keep your password safe and secure!")
 print("Thank you for using the Password Generator!\n")

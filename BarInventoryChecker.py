e905cdd7f229d1ba974e729b1a92ba03b2628693
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-22 02:33:41 +0800

    fix repeadtedly showing menu.

--- BarInventoryChecker.py
+++ BarInventoryChecker.py
@@ -8,48 +8,54 @@ liquor = [
 ]
 stock = [5, 3, 8, 2, 6, 4]
 
-def getMenu():
+def get_menu():
     print("------------------------------")
     print("*** Bar Inventory Checker ***")
     print("------------------------------")
     print("1. View Inventory")
-    print("2. Check Item Stock")
-    print("3. Add Stock")
-    print("4. Remove Stock")
-    print("5. Check Low Stock")
-    print("6. Search Item")
-    print("7. Show Total Items")
-    print("8. Exit")
+    print("2. Add Stock")
+    print("3. Remove Stock")
+    print("4. Check Low Stock")
+    print("5. Search Item")
+    print("6. Show Total Items")
+    print("7. Exit")
     print("\nPlease select an option from the menu above.")
+
+def get_option():
     return int(input("Enter your option: "))
 
+
+get_menu()
+menuOption = get_option()
+
 while True:
-    menuOption = getMenu()
     if menuOption <= 1 or menuOption <= 8:
         if menuOption == 1:
-            print("---> View Inventory <---")
+            print("\n---> View Inventory <---")
             #to show the list together with stocks list
             for i in range(len(liquor)):
                 print(f"{liquor[i]} : {stock[i]}")
-            continue
+            menuOption = get_option()
 
-        elif menuOption == 3:
+        elif menuOption == 2:
             add_list = input("Add Stock: ")
             add_stock = input("How many: ")
             liquor.append(add_list)
             stock.append(add_stock)
             for i in range(len(liquor)):
                 print(f"{liquor[i]} : {stock[i]}")
-            continue
+            menuOption = get_option()
 
-        elif menuOption == 4:
+        elif menuOption == 3:
             remove_list = input("Remove Stock: ")
+            
             liquor.remove(remove_list)
             print("\n".join(liquor))
-    else: 
-        print("Incorrect option!")
-        getMenu()
-    
 
+    else: 
+        print("Incorrect option!\n")
+        get_menu()
+        menuOption = get_option()
+        
 print("Program closed!")
 

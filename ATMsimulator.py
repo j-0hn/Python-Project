c6216af8c3e0ad0b43bb46d51d63997116943984
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-23 13:44:15 +0800

    create functions for check balance, deposit and withdraw add try catch to handle some invalid inputs from user update and remove some codes related to pin validation main code are now more cleaner, all reapeated codes will have functions to handle it

--- ATMsimulator.py
+++ ATMsimulator.py
@@ -13,69 +13,78 @@ def counter(count):
     count += 1
     return count
 
-#To check the PIN if Correct
-pin = input("Enter your PIN: ")
+def get_input_pin():
+    return int(input("Enter your PIN: "))
+
+def check_balance(balance):
+        print(f"\n**** CHECK BALANCE ****\nYour Balance is $ {balance}")
+        if balance <= 0 or balance <= 100:
+                print("Please Deposit in your account!\n")
+        else: print("Thank you for Banking with us!\n")
+        return balance
+
+def get_deposit(prev_balance):
+        print("\n**** DEPOSIT ****")
+        depositCash = int(input("Enter Amount: "))
+        print(f"Total Amount is: {prev_balance + depositCash}\n")
+        prev_balance += depositCash
+        return prev_balance
+
+def get_withdraw(present_bal):
+        withdrawCash = int(input("\nEnter amount to Withdraw: "))
+
+        if present_bal <= 0:
+                print("Insufficient Funds!\n")
+        elif present_bal <= 100:
+                print("Amount Invalid!\n")
+        else:
+                print("\n**** WITHDRAW CASH ****")
+                print(f"The remaining balance is: {present_bal - withdrawCash}\n")
+                present_bal -=withdrawCash
+        return present_bal
+
 
 while attemptCount < 3:
+   try:
+        pin = get_input_pin()
+        if len(str(pin)) != 4:
+                print("\nPIN must be 4 digits!")
+                pin = get_input_pin()
+                attemptCount = counter(attemptCount)
+                print(f"\nAttempt(s): {attemptCount}")
+                
+        elif myPin != pin:
+                print("\nWrong PIN!")
+                pin = get_input_pin()
+                attemptCount = counter(attemptCount)
+                print(f"\nAttempt(s): {attemptCount}")
 
-   if len(pin) != 4:
-        print("\nPIN must be 4 digits!")
-        pin = input("Re-Enter PIN: ")
+        elif len(str(pin)) == 4 and myPin == pin:
+                print("Login Successful!\n")
+                while True:
+                        print("1. Check Balance")
+                        print("2. Deposit")
+                        print("3. Withdraw")
+                        print("4. Exit")
+
+                        option = input("Choose an option: ")
+                        option = int(option)
+                        if option == 1:
+                                myBalance = check_balance(myBalance)
+
+                        elif option == 2:
+                        #to store value of balance and not vanished it
+                                myBalance = get_deposit(myBalance)
+
+                        elif option == 3:
+                                myBalance = get_withdraw(myBalance)
+
+                        elif option == 4:
+                                print("\nGOODBYE! Thank you!")
+                                break
+   except ValueError:
+        print("Please enter a valid number!") 
         attemptCount = counter(attemptCount)
         print(f"\nAttempt(s): {attemptCount}")
 
-   elif not pin.isdigit():
-        print("\nPlease Input Numbers only!")
-        pin = input("Re-Enter PIN: ")
-        attemptCount = counter(attemptCount)
-        print(f"\nAttempt(s): {attemptCount}")
-
-   elif myPin != int(pin):
-        print("\nWrong PIN!")
-        pin = input("Re-Enter PIN: ")
-        attemptCount = counter(attemptCount)
-        print(f"\nAttempt(s): {attemptCount}")
-
-   elif pin.isdigit() and len(pin) == 4:
-        pin = int(pin)
-        print("Login Successful!\n")
-        
-        if myPin == pin:
-
-           while True:
-                print("1. Check Balance")
-                print("2. Deposit")
-                print("3. Withdraw")
-                print("4. Exit")
-
-                option = input("Choose an option: ")
-                option = int(option)
-                if option == 1:
-                        print(f"\n**** CHECK BALANCE ****\nYour Balance is $ {myBalance}")
-                        if myBalance <= 0 or myBalance <= 100:
-                           print("Please Deposit in your account!\n")
-                        else: print("Thank you for Banking with us!\n")
-
-                elif option == 2:
-                        print("\n**** DEPOSIT ****")
-                        depositCash = int(input("Enter Amount: "))
-                        print(f"Total Amount is: {myBalance + depositCash}\n")
-                        myBalance +=depositCash
-
-                elif option == 3:
-                        withdrawCash = int(input("\nEnter amount to Withdraw: "))
-                        
-                        if myBalance <= 0:
-                                print("Insufficient Funds!\n")
-                        elif myBalance <= 100:
-                                print("Amount Invalid!\n")
-                        else:
-                                print("\n**** WITHDRAW CASH ****")
-                                print(f"The remaining balance is: {myBalance - withdrawCash}\n")
-                                myBalance -=withdrawCash
-
-                elif option == 4:
-                        print("\nGOODBYE! Thank you!")
-                        break
-        
 print("Attempts Exceeded!")

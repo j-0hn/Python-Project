93223b41a2388debc4c423768b536698c778bf36
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-25 12:41:01 +0800

    adding while loop to repeat orders created order list[] to store order input and append it created code of sum of all order item

--- CocktailOrdering.py
+++ CocktailOrdering.py
@@ -9,10 +9,12 @@ drinks = [
     {"name" : "Old Fashion", "price" : 128}
 ]
 
+order_list = []
+total = 0
 while True:
     try:
         print("1. View list \n2. Order")
-        option = int(input("\nChoose an option: \n"))
+        option = int(input("\nChoose an option: "))
         if option == 1:
             print("\n*** Cocktails ***")
 
@@ -29,16 +31,30 @@ while True:
                 break
             
         elif option == 2:
-            print("Order when you ready!")
-            for index, myDrink in enumerate(drinks, start = 1):
-                print(index, myDrink["name"], myDrink["price"])
-            choice = int(input("Enter Order: "))
-            my_order = drinks[choice - 1]
-            print("You selected: ", my_order["name"])
-            print("Price is: ", my_order["price"])
+            
+            while True:
+                print("\nOrder when you ready!")
+                for index, myDrink in enumerate(drinks, start = 1):
+                    print(index, myDrink["name"], myDrink["price"])
+                choice = int(input("\nEnter Order: "))
+                drink_name = drinks[choice - 1]
+                #print(f"Order: {drink_name["name"]} : {drink_name["price"]}")
+                
+                order_list.append({"name" : drink_name["name"], "price" : drink_name["price"]})
 
+                order_again = input("Order Again Y or N: ")
+                if order_again == "Y" or order_again == "y":
+                    continue
+                else: 
+                    print("\nYour Order is:")
+                    for index, my_order in enumerate(order_list, start = 1):
+                        print(index, my_order["name"], my_order["price"])
+
+                    for order in order_list:
+                        total += order["price"]
+                    print("Total: ", total)
+                    break
 
     except ValueError:
         print("Input valid number!")
-
 print("Thank you Using Ordering Systen!")

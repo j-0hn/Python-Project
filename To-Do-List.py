7982979e8cfc8fb2e50f5cf39a2f607b694f5068
jOHN <135294734+j-0hn@users.noreply.github.com>
2026-08-27 11:53:37 +0800

    adding add task functionality append a list of task into list[] inside is dictionaries using for loop enumerate to show the list of task

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,9 +1,27 @@
-print("----- TO DO LIST -----")
+print("\n----- TO DO LIST -----")
 
 my_task = []
 
-print("1. Add Task")
-user_task = input("I need to ... ")
-my_task.append(user_task)
-for task in range(len(my_task)):
-    print(my_task[task])
+while True:
+    print("1. Add Task\n2. View Task\n3. Remove Task\n4. Completed")
+    option = int(input("\nWhat option: "))
+
+    if option == 1:
+        user_task = input("\nI need to ... ")
+        user_status = input("Status of task: ")
+
+        my_task.append({"task" : user_task, "status" : user_status})
+
+        #for i in range(len(my_task)):
+         #   print(my_task[i])
+        for index, tasks in enumerate(my_task, start = 1):
+            print("\nMy Task Now!")
+            print(index, tasks["task"], tasks["status"],"\n")
+
+    elif option == 2:
+        print("*** View Task ***")
+    elif option == 3:
+        print("*** Remove Task ***")
+    
+
+print("Task Closed!")

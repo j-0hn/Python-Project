4bb20d559c9282b18b469d251213d942ad534984
jOHN <johnstanford143@gmail.com>
2026-08-30 13:11:16 +0800

    adding function to center the window screen learned how to add labels, inputs and other modules

--- GUI/PassGeneratorGUI.py
+++ GUI/PassGeneratorGUI.py
@@ -1,23 +1,47 @@
 import tkinter as tk
+import random
+import string
 
 window = tk.Tk()
 window.title("Password Generator")
-window.geometry("400x400")
+
+varLetters = string.ascii_letters
+varDigits = string.digits
+varSpecialChars = string.punctuation
+
+passWord = ""
+output_label = ""
+label_for_output = ""
+#this function to run window in the center of screen
+def center_window(window, width, height):
+    #to get the actual pixels of my screen
+    screen_width = window.winfo_screenwidth() 
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    window.geometry(f"{width}x{height}+{x}+{y}")
+center_window(window, 300, 300)
+
+def get_range():
+    password_length = get_input_range.get()
+    label_for_output.config(text="Your Password is...")
+    output_label.config(text=password_length)
+    get_input_range.delete(0, tk.END)
+
+label = tk.Label(window, text="Enter Range Password")
+label.pack(pady=10)
 
 # Create an Entry widget for password input
 get_input_range = tk.Entry(window)
-get_input_range.pack(pady=5)
-
-# Function to get the product value
-def get_range():
-    password_lenght = get_input_range.get()
-    print("Product entered:", password_lenght)
-    get_input_range.delete(0, tk.END)
-
-
+get_input_range.pack()
 
 # Button to trigger the function
 submit_button = tk.Button(window, text="Done", command=get_range)
 submit_button.pack(pady=10)
+
+label_for_output = tk.Label(window, text="")
+label_for_output.pack()
+output_label = tk.Label(window, text="")
+output_label.pack(pady=10)
 
 window.mainloop()

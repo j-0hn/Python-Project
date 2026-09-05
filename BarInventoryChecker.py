979000141dfe14f77788135a96c0ac6d141dd9d7
jOHN <johnstanford143@gmail.com>
2026-09-05 12:48:04 +0800

    fixed the getOption function to acces its input data globally removed repeatedly assigning menuOption data from get option function

--- BarInventoryChecker.py
+++ BarInventoryChecker.py
@@ -3,6 +3,7 @@ import json
 with open("jsonFile/liqour.json", "r") as file:
     liquor = json.load(file)
 
+menuOption = None
 def get_menu():
     print("------------------------------")
     print("*** Bar Inventory Checker ***")
@@ -15,13 +16,30 @@ def get_menu():
     print("6. Show Total Items")
     print("7. Exit")
     print("\nPlease select an option from the menu above.")
+    get_option()
 
 def get_option():
-    return int(input("Enter your option: "))
+    global menuOption
+    menuOption = int(input("Enter your option: "))
+    return
 
 def display_list():
     for index, item in enumerate(liquor):
         print(f"{index + 1}. {item['name']}: {item['stock']}")
+    get_option()
+
+def add_item():
+    add_item = input("Add Stock: ")
+    add_stock = input("How many: ")
+
+    liquor.append({
+        "name": add_item,
+        "stock": int(add_stock)
+                })
+
+    write_to_file()
+    display_list()
+    get_option()
 
 def remove_item():
     remove_item = input("Remove Stock: ")
@@ -35,14 +53,16 @@ def remove_item():
 
     write_to_file() 
     print(f"\nYou removed -> {remove_item['name']}: {remove_item['stock']}\n")
+    display_list()
+    get_option()
     
-    
-
 def check_low_stock():
     lowest_stock = min(item['stock'] for item in liquor)
     for item in liquor:
         if item['stock'] == lowest_stock:
             print(f"{item['name']} is the lowest stock: {item['stock']}")
+
+    get_option()
 
 def search_item():
     search_item = input("Search Item: ")
@@ -52,60 +72,46 @@ def search_item():
     else:
         print(f"{search_item} is not in the inventory.")
 
+    get_option()
+
 def total_items():
     total_items = sum(item['stock'] 
     for item in liquor)
     print(f"Total Items in Stock: {total_items}")
     print(f"Total Item: {len(liquor)}")
 
+    get_option()
+
 def write_to_file():
     with open("jsonFile/liqour.json", "w") as file:
         json.dump(liquor, file, indent=2)
             
 get_menu()
-menuOption = get_option()
+
 while True:
    try:
     
     if menuOption >= 1 and menuOption <= 7:
         if menuOption == 1:
             print("\n---> View Inventory <---")
-            #to show the list together with stocks list
             display_list()
-            #for i in range(len(liquor)):
-             #   print(f"{liquor[i]} : {stock[i]}")
-            menuOption = get_option()
+            #menuOption = get_option()
 
         elif menuOption == 2:
-            add_item = input("Add Stock: ")
-            add_stock = input("How many: ")
-
-            liquor.append({
-                "name": add_item,
-                "stock": int(add_stock)
-                        })
-
-            write_to_file()
-            display_list()
-            menuOption = get_option()
-
+            add_item()
+           
         elif menuOption == 3:
             remove_item()
-            display_list()
-            menuOption = get_option()
-
+           
         elif menuOption == 4:
             check_low_stock()
-            menuOption = get_option()
-
+           
         elif menuOption == 5:
             search_item()
-            menuOption = get_option()
-
+           
         elif menuOption == 6:
             total_items()
-            menuOption = get_option()
-            
+           
         elif menuOption == 7:
             break
 

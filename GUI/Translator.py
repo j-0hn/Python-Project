8e73d22a3609dd06055c0de955a099ebef1ea39b
jOHN <johnstanford143@gmail.com>
2026-09-06 13:24:46 +0800

    fix the center function no show, i must forget the .mainloo()

--- GUI/Translator.py
+++ GUI/Translator.py
@@ -3,7 +3,7 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Translator")
 
-# This function will make our window center on the screen
+# This function will make the window center on the screen
 def center_window(root, width, height):
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -11,3 +11,7 @@ def center_window(root, width, height):
     y = (screen_height - height) // 2
     root.geometry(f"{width}x{height}+{x}+{y}")
 center_window(root, 400, 400)
+
+
+
+root.mainloop()

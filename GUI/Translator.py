c1a9230a910c6c48cb3d815c7a04a4f6ea12d188
jOHN <johnstanford143@gmail.com>
2026-09-07 14:09:07 +0800

    ading function for my translate button and i created a json file for my translation list

--- GUI/Translator.py
+++ GUI/Translator.py
@@ -12,6 +12,11 @@ def center_window(root, width, height):
     root.geometry(f"{width}x{height}+{x}+{y}")
 center_window(root, 400, 400)
 
+def translate_text(english_text):
+    chinese_text = "haihai"
+    output_text.delete("1.0", tk.END)
+    output_text.insert("1.0", chinese_text)
+
 label = tk.Label(root, text="Enter English text to translate to Chinese:", font=("Arial", 12))
 label.pack(pady=10)
 
@@ -25,7 +30,7 @@ translate_button.pack(pady=10)
 arrow = tk.Label(root, text="↓", font=("Arial", 25))
 arrow.pack()
 
-# Chinese Output field
+# Chinese Output Text field
 output_text = tk.Text(root, height=8, width=40)
 output_text.pack(pady=10)
 

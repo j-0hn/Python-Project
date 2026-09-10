1d38d13b28d5e0b36a324e5b2aad5198a2cef6df
jOHN <johnstanford143@gmail.com>
2026-09-10 12:03:23 +0800

    already coded the flow, next is need to give functionality

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -4,4 +4,23 @@ print("------------------------------\n**** Mini Expense Tracker ****\n---------
 "\n 3. Calculate Expense"
 "\n 4. Exit \n ------------------------------")
 
-print(int(input("Enter Option: ")))
+def option():
+    print("1. Add, 2. View, 3. Calculate, 4. Exit\n")
+
+def get_option():
+   return int(input("Enter Option: "))
+
+while True:
+    num_option = get_option()
+
+    if num_option == 1:
+        print("add")
+    elif num_option == 2:
+        print("view")
+    elif num_option == 3:
+        print("calculate")
+    else:
+        break
+
+
+print("Program Closed!")

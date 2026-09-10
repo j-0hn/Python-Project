e9747b381034c6666bd5d47acdd8b1bdde87cf47
jOHN <johnstanford143@gmail.com>
2026-09-10 11:50:58 +0800

    creating another mini project mini expense tracker

---
+++ ExpenseTracker.py
@@ -0,0 +1,7 @@
+print("------------------------------\n**** Mini Expense Tracker ****\n------------------------------" 
+"\n 1. Add Expense" 
+"\n 2. View Expense"
+"\n 3. Calculate Expense"
+"\n 4. Exit \n ------------------------------")
+
+print(int(input("Enter Option: ")))

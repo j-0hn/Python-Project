052b457e330a060e74166f0ce01c8ad3d1640928
jOHN <johnstanford143@gmail.com>
2026-09-15 22:28:41 +0800

    practice how data is wroking in class

--- TestFile.py
+++ TestFile.py
@@ -1,12 +1,21 @@
 class student():
-    def __init__(self, name, age, grade):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.grade = grade
 
-    def get_grade(self):
-        return self.name
+    def display(self):
+        print(f"{self.name} {self.age}")
+        
 
+s1 = student("John", 20)
+s2 = student("Isabek", 19)
+s3 = student("Bianca", 20)
 
-student_one = student(88)
+s1.display()
+s2.display()
+s3.display()
+
+#print(student_one.name)
+#print(student_one.age)
+
 

4096dc7b65b393e6f01ac0d303a15159e90edfbc
jOHN <johnstanford143@gmail.com>
2026-09-18 12:21:09 +0800

    change my project to BMI calculator

---
+++ GUI/calculator.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Python Calculator")
+
+# This function will make the window center on the screen
+def center_window(root, width, height):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    root.geometry(f"{width}x{height}+{x}+{y}")
+center_window(root, 400, 400)
+
+
+
+root.mainloop()

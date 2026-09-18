ebfac695b7ef275a8f1cf0cde252da669c6a1070
jOHN <johnstanford143@gmail.com>
2026-09-18 13:14:01 +0800

    fix the calculation formula for BMI i study the pack and grid to diaplay in in window nnext is adding some input validation

--- GUI/BMIcalculator.py
+++ GUI/BMIcalculator.py
@@ -12,7 +12,64 @@ def center_window(root, width, height):
     root.geometry(f"{width}x{height}+{x}+{y}")
 center_window(root, 400, 400)
 
+def calculate():
+    weight = float(inputWeight.get())
+    height = float(inputHeight.get()) / 100
+
+    bmi = weight / (height * height)
+    output_label.config(text=f"{bmi: .1f}")
 
 
+label_weight = tk.Label(root, text="Weight in (kg)")
+label_weight.pack()
+
+inputWeight = tk.Entry(root)
+inputWeight.pack()
+
+label_height = tk.Label(root, text="Height in (cm)")
+label_height.pack()
+
+inputHeight = tk.Entry(root)
+inputHeight.bind("<Return>", lambda event: calculate())
+inputHeight.pack()
+
+calc_button = tk.Button(root, text="Calculate", command=calculate)
+calc_button.pack()
+
+output_label = tk.Label(root, text="")
+output_label.pack()
+
+table = tk.Frame(root)
+table.pack()
+
+label1 = tk.Label(table, text="BMI")
+label1.grid(row=0, column=0)
+
+label2 = tk.Label(table, text="Category")
+label2.grid(row=0, column=1)
+
+label3 = tk.Label(table, text="< 18.5")
+label3.grid(row=1, column=0)
+
+label4 = tk.Label(table, text="Underweight")
+label4.grid(row=1, column=1)
+
+label5 = tk.Label(table, text="18.5–24.9")
+label5.grid(row=2, column=0)
+
+label6 = tk.Label(table, text="Normal weight")
+label6.grid(row=2, column=1)
+
+label7 = tk.Label(table, text="25.0–29.9")
+label7.grid(row=3, column=0)
+
+label8 = tk.Label(table, text="Overweight")
+label8.grid(row=3, column=1)
+
+label9 = tk.Label(table, text="≥ 30")
+label9.grid(row=4, column=0)
+
+label10 = tk.Label(table, text="Obese")
+label10.grid(row=4, column=1)
 
 root.mainloop()
